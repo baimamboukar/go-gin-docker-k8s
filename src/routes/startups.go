d2b3b6c3bbaab8b49f79ce619c6b40b66fffd277
baimamboukar <baimamboukar@gmail.com>
2023-12-23 00:56:16 +0100

    chore: add routes setup and group setup

---
+++ src/routes/startups.go
@@ -0,0 +1,42 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func startupsGroupRouter() {
+	baseRouter := setupRoutes()
+	startups := baseRouter.Group("/startups")
+
+	startups.GET("/all", GetAllStartups)
+	startups.GET("/:id", GetStartupByID)
+	startups.POST("/create", CreateStartup)
+	startups.PATCH("/update", UpdateStartup)
+	startups.PUT("/update", UpdateStartup)
+	startups.DELETE("/:id", DeleteStartup)
+}
+
+func GetAllStartups(c *gin.Context) {
+	// Implement logic to get all startups
+	c.JSON(200, gin.H{"message": "Get all startups"})
+}
+
+func GetStartupByID(c *gin.Context) {
+	// Implement logic to get a startup by ID
+	startupID := c.Param("id")
+	c.JSON(200, gin.H{"message": "Get startup by ID", "id": startupID})
+}
+
+func CreateStartup(c *gin.Context) {
+	// Implement logic to create a new startup
+	c.JSON(200, gin.H{"message": "Create a new startup"})
+}
+
+func UpdateStartup(c *gin.Context) {
+	// Implement logic to update a startup
+	c.JSON(200, gin.H{"message": "Update a startup"})
+}
+
+func DeleteStartup(c *gin.Context) {
+	// Implement logic to delete a startup by ID
+	startupID := c.Param("id")
+	c.JSON(200, gin.H{"message": "Delete startup by ID", "id": startupID})
+}
