e5481b33033e1f301cc70eeaed5245ec4427127d
baimamboukar <baimamboukar@gmail.com>
2023-12-22 16:10:16 +0100

    init: setup `gin-mongo-dockerk8s` app

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/baimamboukar/go-gin-docker-k8s
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("---Starting with GIN-DOCKER-K8s--------")
+}
