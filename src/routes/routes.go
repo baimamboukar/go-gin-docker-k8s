b3ca22b8188f339fa84d5c517a703dfb2e7d38c1
baimamboukar <baimamboukar@gmail.com>
2023-12-23 02:16:47 +0100

    chore: rearrangr routes file

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -2,12 +2,12 @@ package routes
 
 import "github.com/gin-gonic/gin"
 
-func setupRoutes() {
+func SetupRoutes() *gin.Engine {
 	r := gin.Default()
 
 	versionRouter := r.Group("/api/v1")
 	startupsGroupRouter(versionRouter)
 	companiesGroupRouter(versionRouter)
 
-	r.Run(":8081")
+	return r
 }
