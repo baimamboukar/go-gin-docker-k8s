9919bd2c9923fe7f83437d969b7a15db89907961
baimamboukar <baimamboukar@gmail.com>
2023-12-26 14:18:31 +0100

    chore: update models to match `gorm migrations`

--- src/models/company.go
+++ src/models/company.go
@@ -1,17 +1,19 @@
 package models
 
+import "gorm.io/gorm"
+
 type Company struct {
+	gorm.Model
 	ID          string      `json:"id"`
 	Name        string      `json:"name"`
 	LogoURL     string      `json:"logo_url"`
 	WebsiteURL  string      `json:"website_url"`
-	SocialMedia SocialMedia `json:"social_media"`
+	SocialMedia SocialMedia `json:"social_media"  gorm:"embedded"`
 	Slogan      string      `json:"slogan"`
 	Industry    string      `json:"industry"`
 	CEO         string      `json:"ceo"`
 	Since       string      `json:"since"`
-	Offices     []Office    `json:"offices"`
-	Headquater  Office      `json:"headquarter"`
+	Headquater  Office      `json:"headquarter" gorm:"embedded"`
 }
 
 type Office struct {
