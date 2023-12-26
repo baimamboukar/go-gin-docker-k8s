b75b851d10daf9c55f044e11e47f343cfe50c507
baimamboukar <baimamboukar@gmail.com>
2023-12-26 10:12:44 +0100

    chore: add `db conn` and `middlewares config`

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
-import "github.com/baimamboukar/go-gin-docker-k8s/src/routes"
+import (
+	"github.com/baimamboukar/go-gin-docker-k8s/src/middlewares"
+	"github.com/baimamboukar/go-gin-docker-k8s/src/models"
+	"github.com/baimamboukar/go-gin-docker-k8s/src/routes"
+)
 
 func main() {
 	router := routes.SetupRoutes()
+	models.OpenDatabaseConnection()
+	middlewares.RegisterMiddlewares(router)
 	router.Run(":8081")
 
 }
