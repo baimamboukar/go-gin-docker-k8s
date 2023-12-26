7e24d67f8cb2fb105c8b00b2cf688a4584749c32
baimamboukar <baimamboukar@gmail.com>
2023-12-26 14:17:18 +0100

    chore: setup `db connection`

--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"github.com/baimamboukar/go-gin-docker-k8s/src/middlewares"
 	"github.com/baimamboukar/go-gin-docker-k8s/src/models"
 	"github.com/baimamboukar/go-gin-docker-k8s/src/routes"
+	"github.com/baimamboukar/go-gin-docker-k8s/src/utils"
 )
 
 func main() {
+	utils.LoadEnv()
 	router := routes.SetupRoutes()
 	models.OpenDatabaseConnection()
+	models.AutoMigrateModels()
 	middlewares.RegisterMiddlewares(router)
 	router.Run(":8081")
 
