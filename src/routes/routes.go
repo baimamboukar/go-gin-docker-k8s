c3e6b168fa393415886780b549a9b68e941a2a7e
baimamboukar <baimamboukar@gmail.com>
2023-12-27 20:28:11 +0100

    refactor: refactor routes

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -1,11 +1,15 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
 
 func SetupRoutes() *gin.Engine {
 	r := gin.Default()
 
 	versionRouter := r.Group("/api/v1")
+	versionRouter.GET("/metrics", gin.WrapH(promhttp.Handler()))
 	startupsGroupRouter(versionRouter)
 	companiesGroupRouter(versionRouter)
 
