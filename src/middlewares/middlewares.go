8274128b3481086fe33f976e6d2ec14112efb901
baimamboukar <baimamboukar@gmail.com>
2023-12-28 22:09:23 +0100

    chore: rm prometheus los

--- src/middlewares/middlewares.go
+++ src/middlewares/middlewares.go
@@ -1,15 +1,11 @@
 package middlewares
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterMiddlewares(router *gin.Engine) {
 	//router.Use(AuthMiddleware())
 	router.Use(LoggerMiddleware())
-	fmt.Println("---Registrating Prometheus----")
 	router.Use(PrometheusMiddleware())
-	fmt.Println("---Prometheus Registrated----")
 }
