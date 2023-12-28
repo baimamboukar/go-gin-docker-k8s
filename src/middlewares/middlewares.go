ba4c879da035660bee6f7d3eb40251381049fe43
baimamboukar <baimamboukar@gmail.com>
2023-12-28 01:12:37 +0100

    chore: added `middleware` configs

--- src/middlewares/middlewares.go
+++ src/middlewares/middlewares.go
@@ -7,7 +7,7 @@ import (
 )
 
 func RegisterMiddlewares(router *gin.Engine) {
-	router.Use(AuthMiddleware())
+	//router.Use(AuthMiddleware())
 	router.Use(LoggerMiddleware())
 	fmt.Println("---Registrating Prometheus----")
 	router.Use(PrometheusMiddleware())
