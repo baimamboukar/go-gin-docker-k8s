4bcf9e9baa534af1bf1a0db1852751cc738ce94e
baimamboukar <baimamboukar@gmail.com>
2023-12-29 01:31:35 +0100

    chore: registrated `zap` middlewares

--- src/middlewares/middlewares.go
+++ src/middlewares/middlewares.go
@@ -1,11 +1,18 @@
 package middlewares
 
 import (
+	"time"
+
+	ginzap "github.com/gin-contrib/zap"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func RegisterMiddlewares(router *gin.Engine) {
+	logger, _ := zap.NewProduction()
 	//router.Use(AuthMiddleware())
 	router.Use(LoggerMiddleware())
 	router.Use(PrometheusMiddleware())
+	router.Use(ginzap.Ginzap(logger, time.RFC3339, true))
+	router.Use(ginzap.RecoveryWithZap(logger, true))
 }
